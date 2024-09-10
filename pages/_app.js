076dba64ede4012362660a2e984dff5e0b769575
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-09-10 15:04:43 +0200

    Add SWR config

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <SWRConfig value={{fetcher}}> 
       <GlobalStyle />
       <Component {...pageProps} />
+    </SWRConfig>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art"
+  );
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Art Gallery App</h1>
     </div>
   );
 }
