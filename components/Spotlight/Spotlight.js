ea2ea6a77320961c9bff0b1b673b535b270de487
AntoineFachez <anthony.zornig@gmx.de>
2024-09-11 09:05:28 +0200

    move Spotlight Component to own Folder in /components

---
+++ components/Spotlight/Spotlight.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+const Root = styled.section`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  padding: 1rem;
+  gap: 1rem;
+  background: ${(props) =>
+    props.colors && `linear-gradient(0.25turn, ${props.colors.join(", ")}) `};
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Piece = styled.div`
+  flex: 1;
+  padding: 1rem;
+`;
+
+const Artist = styled.div`
+  width: 300px;
+  text-align: center;
+  height: 100%;
+  padding: 1rem;
+  color: black;
+`;
+
+export default function Spotlight({ pieces }) {
+  const randomPiece = pieces[Math.floor(Math.random() * pieces.length)];
+
+  return (
+    <Root colors={randomPiece.colors}>
+      <Piece>
+        <Image
+          src={randomPiece.imageSource}
+          alt="spotlight piece"
+          width={randomPiece.dimensions.width * 0.2}
+          height={randomPiece.dimensions.height * 0.2}
+        />
+      </Piece>
+      <Artist>{randomPiece.artist}</Artist>
+    </Root>
+  );
+}
