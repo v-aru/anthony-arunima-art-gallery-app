fb60eef458f5d71d6af80fa9a055d99168ba6c62
AntoineFachez <anthony.zornig@gmx.de>
2024-09-12 12:16:41 +0200

    introduce spotlightStyles.js, refactor Spotlight by importing styles

--- components/Spotlight/Spotlight.js
+++ components/Spotlight/Spotlight.js
@@ -1,38 +1,20 @@
 import ArtPiecesPreview from "@/components/ArtPiecesPreview/ArtPiecesPreview";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import styled from "styled-components";
 import Image from "next/image";
 import Favourites from "../Favourites/Favourites";
+import { Root, Piece, Artist } from "./spotlightStyles";
 
-const Root = styled.section`
-  display: flex;
-  flex: 1;
-  align-items: center;
-  padding: 1rem;
-  gap: 1rem;
-  background: ${(props) =>
-    props.colors && `linear-gradient(0.25turn, ${props.colors.join(", ")}) `};
-  flex-flow: column wrap;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Piece = styled.div`
-  flex: 1;
-  padding: 1rem;
-  position: relative;
-`;
-
-const Artist = styled.div`
-  width: 300px;
-  text-align: center;
-  height: 100%;
-  padding: 1rem;
-  color: black;
-`;
-
-export default function Spotlight({ image, artist, colors, width, height, slug, artPieceInfo, onToggleFavourite}) {
+export default function Spotlight({
+  image,
+  artist,
+  colors,
+  width,
+  height,
+  slug,
+  artPieceInfo,
+  onToggleFavourite,
+}) {
   // const router = useRouter();
   // const handleClick = () => {
   //   router.push("/"); // Navigate back to the homepage
---
+++ components/Spotlight/spotlightStyles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+export const Root = styled.section`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  padding: 1rem;
+  gap: 1rem;
+  background: ${(props) =>
+    props.colors && `linear-gradient(0.25turn, ${props.colors.join(", ")}) `};
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Piece = styled.div`
+  flex: 1;
+  padding: 1rem;
+  position: relative;
+`;
+
+export const Artist = styled.div`
+  width: 300px;
+  text-align: center;
+  height: 100%;
+  padding: 1rem;
+  color: black;
+`;
