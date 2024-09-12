c067d8cdd62eaa79b70d6a296bc2f22557af5e75
AntoineFachez <anthony.zornig@gmx.de>
2024-09-12 12:44:15 +0200

    refactor pages/index.j, pages/art-pieces/index, Spotlight.js to use Title.js Component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 import Spotlight from "../components/Spotlight/Spotlight";
 import { useEffect, useState } from "react";
+import Title from "@/components/Title/Title";
 
 const Root = styled.main`
   color: #888;
 `;
 
-const Title = styled.h1`
-  text-align: center;
-  color: #888;
-`;
-
 //Renaming HomePage to SpotlightPage
-export default function SpotlightPage({ data, artPieceInfo, onToggleFavourite }) {
+export default function SpotlightPage({
+  data,
+  artPieceInfo,
+  onToggleFavourite,
+}) {
   const [randomPiece, setRandomPiece] = useState(null);
 
   useEffect(() => {
@@ -23,22 +23,22 @@ export default function SpotlightPage({ data, artPieceInfo, onToggleFavourite })
       const selectedPiece = data[randomNumber];
       setRandomPiece(selectedPiece);
     }
-  }, [data]); 
+  }, [data]);
 
   if (!randomPiece) return <div>Loading...</div>;
 
   return (
     <Root>
-      <Title>ART GALLERY - SPOTLIGHT</Title>
-      <Spotlight 
+      <Title content={"ART GALLERY - SPOTLIGHT"} />
+
+      <Spotlight
         image={randomPiece.imageSource}
         colors={randomPiece.colors}
         artist={randomPiece.artist}
         width={randomPiece.dimensions.width}
-        height={randomPiece.dimensions.height} 
+        height={randomPiece.dimensions.height}
         artPieceInfo={artPieceInfo}
         onToggleFavourite={onToggleFavourite}
-        
       />
     </Root>
   );
