431ac5bb2f64894601e93dd0734e68dfa7c6a4c0
AntoineFachez <anthony.zornig@gmx.de>
2024-09-13 18:57:57 +0200

    introduce useResizeObserver

---
+++ hooks/useResizeObserver.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+
+const useResizeObserver = (
+  nestedRef,
+  setDimensions,
+  handleUserActivity,
+  setIsUserActive
+) => {
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.target === nestedRef.current) {
+          // Get the new dimensions of the parent element
+          const { width, height } = entry.contentRect;
+
+          // Set the dimensions of the globe component
+          setDimensions({ width, height });
+        }
+      }
+    });
+
+    resizeObserver.observe(nestedRef.current);
+    const timeoutId = setTimeout(() => {
+      setIsUserActive(false);
+    }, 10);
+
+    document.addEventListener("mousemove", handleUserActivity);
+
+    return () => {
+      resizeObserver.disconnect();
+      clearTimeout(timeoutId);
+      document.removeEventListener("mousemove", handleUserActivity);
+    };
+  }, [handleUserActivity]);
+};
+
+export default useResizeObserver;
