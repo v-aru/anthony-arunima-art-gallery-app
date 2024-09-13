dd34b36087dfcbe254a5f4ecc43f2d1aa9618cbc
AntoineFachez <anthony.zornig@gmx.de>
2024-09-13 19:02:58 +0200

    import useCallBack hook

--- components/p5/kineticTypography/Index.js
+++ components/p5/kineticTypography/Index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import dynamic from "next/dynamic";
 import useResizeObserver from "@/hooks/useResizeObserver.js";
