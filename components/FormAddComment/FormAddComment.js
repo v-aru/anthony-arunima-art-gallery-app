fc70cc99e782bd0df96b5e6b7b4641eb9ac1915d
AntoineFachez <anthony.zornig@gmx.de>
2024-09-14 01:37:09 +0200

    introduce textarea instead of input, retouch styling, introduce randomLikesAmountGenerator in mockCommentsData

--- components/FormAddComment/FormAddComment.js
+++ components/FormAddComment/FormAddComment.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useContext, useEffect, useRef } from "react";
+import {
+  StyledFormAddComment,
+  TextArea,
+  FormAddCommentFooter,
+  Button,
+} from "./formAddCommentStyling";
+import CommentsContext from "../CommentsList/CommentsContext";
 
 export default function FormAddComment() {
+  const textAreaRef = useRef(null);
+  const { filteredCommentsBySlug } = useContext(CommentsContext);
+  console.log(filteredCommentsBySlug?.length);
+
+  useEffect(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = "auto";
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+    }
+  }, []);
+
+  const handleInput = (e) => {
+    e.target.style.height = "auto";
+    e.target.style.height = `${e.target.scrollHeight}px`;
+  };
   return (
-    <div>
-      <div>FormAddComment</div>
-      <input type="text" />
-    </div>
+    <StyledFormAddComment>
+      <div>{filteredCommentsBySlug?.length} Comments</div>
+
+      <label className="input-sizer stacked">
+        <TextArea
+          ref={textAreaRef}
+          onInput={handleInput}
+          rows="1"
+          placeholder="comment"
+        ></TextArea>
+      </label>
+      <FormAddCommentFooter>
+        <Button>Comment</Button>
+      </FormAddCommentFooter>
+    </StyledFormAddComment>
   );
 }
